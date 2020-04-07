2e2c3e712d6c54aa6809af758d078c50652975c6
Art <bachmann.art@gmail.com>
2020-04-07 21:32:08 +0300

    .env and api key completed

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,20 +1,17 @@
 import React from 'react';
-const conditions = ({ responseObj }) => {
+import classes from './Conditions.module.css'
+
+const conditions = ({ error, loading, responseObj }) => {
   return (
-    <div>
+    <div className={classes.Wrapper}>
+      {error && <small className={classes.Small}>Please enter a valid city.</small>}
+      {loading && <div className={classes.Loader}>Loading...</div>}
       {responseObj.cod === 200 ?
         <div>
           <p><strong>{responseObj.name}</strong></p>
-          <p>It is currently
-            {Math.round(responseObj.main.temp)} degrees out with
-            {responseObj.weather[0].description}  and wind speed is  {responseObj.wind.speed} m/s.
-            in the country {responseObj.sys.country}
-            and other data: <br />
-            {responseObj.main.humidity} <br />
-            {responseObj.main.pressure} <br />
-            {responseObj.main.feels_like} <br />
-            {responseObj.main.temp} <br />
-
+          <p>It is currently &nbsp;
+             {Math.round(responseObj.main.temp)} degrees out with &nbsp;
+            {responseObj.weather[0].description}  and wind speed is about {responseObj.wind.speed} m/s.
           </p>
         </div>
         : null
